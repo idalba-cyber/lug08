a56ca029a8db06403f5c9f1d2df1f29df6157218
Isabella <67A@A67>
2026-07-17 12:05:23 +0200

    operatori unari

---
+++ src/operatoriUnari/IncrementoDecremento.java
@@ -0,0 +1,15 @@
+package operatoriUnari;
+
+public class IncrementoDecremento {
+	public int i = 0;
+	
+	public void calcoli1() {
+		System.out.println(++i + " " + i++ + " " + i);
+		
+	}
+	
+	public void calcoli2() {
+		System.out.println(--i + " " + i-- + " " + i);
+	}
+
+}
---
+++ src/operatoriUnari/IncrementoDecrementoTest.java
@@ -0,0 +1,14 @@
+package operatoriUnari;
+
+public class IncrementoDecrementoTest {
+
+	public static void main(String[] args) {
+		IncrementoDecremento my = new IncrementoDecremento();
+		my.i=15;
+		my.calcoli1();
+		my.i = 32;
+		my.calcoli2();
+
+	}
+
+}
